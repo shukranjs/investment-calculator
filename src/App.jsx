e3d4c8236bd818b2955e81fcfc39efa97ad2ca97
shukranjsp@gmail.com <shukranjsp@gmail.com>
2024-02-27 21:39:42 +0100

    feature: added results component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+import { useState } from "react";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputIsValid && <p className="center">Please enter a duration greater or equal than 1</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
